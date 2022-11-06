010d51d9196462b0fa718fcbf8dc68b8dedf4ad0
Sergei Parshin <Sergei.A.P@yandex.com>
2022-11-06 18:36:33 +0300

    ADD auth business logic

--- src/accounts/settings.py
+++ src/accounts/settings.py
@@ -9,7 +9,11 @@ class Settings(BaseSettings):
     server_host: str = '127.0.0.1'
     server_port: int = 8000
     database_url: str = 'sqlite:///./src/database.sqlite3'
-    
+
+    jwt_secret: str
+    jwt_algorithm: str = 'HS256'
+    jwt_expiration: int = 3600  # in seconds
+
     class Config:
         """Config to set .env file uploading"""
         env_file = './src/app/.env'
